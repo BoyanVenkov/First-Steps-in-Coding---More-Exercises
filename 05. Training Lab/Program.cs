ab0ea72f17e29a3d19fbaa634b620cb5ac86a736
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-08 17:39:00 +0300

    Added a New Project

---
+++ 05. Training Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿double length = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+
+double lengthCm = length * 100;
+double widthCm = width * 100 - 100;
+
+double desksPerRow = Math.Floor(widthCm / 70);
+double rows = Math.Floor(lengthCm / 120);
+
+double totalDesks = desksPerRow * rows - 3;
+
+Console.WriteLine(totalDesks);
