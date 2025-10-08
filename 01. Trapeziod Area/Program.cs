6bb615e8296db05cb2da837e1387ed55da0e698e
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-08 17:03:01 +0300

    Add project files.

---
+++ 01. Trapeziod Area/Program.cs
@@ -0,0 +1,5 @@
+﻿double sideA = double.Parse(Console.ReadLine());
+double sideB = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"{(sideA + sideB)* height / 2:f2}");
