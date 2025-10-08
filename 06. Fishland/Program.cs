b335fceca061437294ce58949f66099825981c72
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-08 17:47:49 +0300

    Added a New Project

---
+++ 06. Fishland/Program.cs
@@ -0,0 +1,12 @@
+﻿double mackarelKg = double.Parse(Console.ReadLine());
+double spratKg = double.Parse(Console.ReadLine());
+double palamudKg = double.Parse(Console.ReadLine());
+double safridKg = double.Parse(Console.ReadLine());
+double musselKg = double.Parse(Console.ReadLine());
+
+double PalamudPrice = mackarelKg * 1.6;
+double safridPrice = spratKg * 1.8;
+
+double totalPrice = (palamudKg * PalamudPrice) + (safridKg * safridPrice) + (musselKg * 7.50);
+
+Console.WriteLine($"{totalPrice:f2}");
