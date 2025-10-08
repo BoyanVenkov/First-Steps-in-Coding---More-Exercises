e49cc1d741bbb699ae6c5cae9f1b1284b0120137
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-08 17:17:06 +0300

    Added a New Project

---
+++ 04. Vegetable Market/Program.cs
@@ -0,0 +1,8 @@
+﻿double vegPrice = double.Parse(Console.ReadLine());
+double fruitPrice = double.Parse(Console.ReadLine());
+double vegKg = double.Parse(Console.ReadLine());
+double fruitKg = double.Parse(Console.ReadLine());
+
+double euro = ((vegPrice * vegKg) + (fruitPrice * fruitKg)) / 1.94;
+
+Console.WriteLine($"{euro:f2}");
