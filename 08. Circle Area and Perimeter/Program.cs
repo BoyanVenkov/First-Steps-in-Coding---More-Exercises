c5bd86b9423fff8126ec6f849fcbdf7ea8cad609
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-09 22:25:14 +0300

    Added a New Project

---
+++ 08. Circle Area and Perimeter/Program.cs
@@ -0,0 +1,8 @@
+﻿double r = double.Parse(Console.ReadLine());
+
+double area = Math.PI * Math.Pow(r, 2);
+
+double perimeter = 2 * Math.PI * r;
+
+Console.WriteLine($"{area:f2}");
+Console.WriteLine($"{perimeter:f2}");
